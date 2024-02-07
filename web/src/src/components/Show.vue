99c57faf24a68a332ef820b05a29f384d93701b4
dy <ellendis1124@gmail.com>
2024-02-07 23:35:58 +0900

    学習メモ参照画面のvueに記述していた関数を別ファイルに切り出した

--- web/src/src/components/Show.vue
+++ web/src/src/components/Show.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import axios from "axios";
 import { reactive } from "vue";
 import { onMounted } from "vue";
-import { URL } from "@/constants";
 import type { Memos } from "@/types/Memos";
-import type { Memo } from "@/types/Memo";
+import { useComposition } from "@/composition"
 
-const PAGE_LIMIT = 10
+const { update_page, init_memos, clickSearchButton } = useComposition();
 
 const memos = reactive<Memos>({
   open_page_num: 1,
@@ -15,83 +13,13 @@ const memos = reactive<Memos>({
   max_page_num: 1
 })
 
-const calc_max_page_num = function (result_all) {
-  return Math.ceil(result_all.length / PAGE_LIMIT)
-}
-
-const get_page_record = function (result_all: Memo[], open_page_num: number) {
-  const record_count = result_all.length
-
-  // 最大ページ数
-  const max_page_num = Math.ceil(record_count / PAGE_LIMIT)
-  // 存在しないページ番号が押下されたとき
-  if (open_page_num > max_page_num) {
-    // pass
-  }
-  // ページネーションが発生するとき
-
-  // 取得するレコードの開始地点
-  const pagenation_start = (open_page_num - 1) * PAGE_LIMIT
-
-  // 指定したページ番号で表示するレコード
-  const this_page_record = result_all.slice(pagenation_start, pagenation_start + PAGE_LIMIT)
-
-  return this_page_record
-}
-
-// 押下されたページ番号に対して、表示するレコードを切り替える
-const update_page = async (result_all, open_page_num) => {
-  // 表示対象となるレコードを更新する
-  memos.this_page_record = await get_page_record(result_all, open_page_num)
-  // 押下されているページ番号の情報を更新する
-  memos.open_page_num = open_page_num
-}
-
-
-
 // 画面項目
 const search_word = reactive({
   subject_name: ""
 })
 
-const getData = async () => {
-  let result = await axios.get(URL.MEMO_GET);
-  return result
-};
-
-// 検索処理
-const searchData = async () => {
-  let result = await axios.get(URL.MEMO_GET, {
-    params: {
-      subject_name: search_word.subject_name
-    }
-  });
-  return result
-};
-
-// 検索ボタンを押下したときの処理
-const clickSearchButton = async () => {
-  const result = await searchData()
-  memos.result_all = result.data
-  console.log("result_all_clicksearchbutton", memos.result_all)
-  const page_max_num = await calc_max_page_num(memos.result_all)
-  console.log("pagemaxnum", page_max_num)
-  update_page(memos.result_all, 1)
-  memos.max_page_num = page_max_num
-
-};
-
-const init_memos = async () => {
-  const result = await getData()
-  memos.result_all = result.data
-  update_page(memos.result_all, 1)
-  const page_max_num = await calc_max_page_num(memos.result_all)
-  console.log(page_max_num)
-  memos.max_page_num = page_max_num
-}
-
 onMounted(async () => {
-  await init_memos()
+  await init_memos(memos)
 })
 
 </script>
@@ -100,25 +28,8 @@ onMounted(async () => {
   <div class="show_content">
     <p>
       <input type="text" v-model="search_word.subject_name">
-      <button @click="clickSearchButton">search</button>
+      <button @click="clickSearchButton(memos, search_word)">search</button>
     </p>
-    <!-- {{ memos.max_page_num }} -->
-    <!-- <div class="memo_container">
-      <div class="row header">
-        <div class="cell">項目名</div>
-        <div class="cell">カテゴリー</div>
-        <div class="cell">学習時間</div>
-        <div class="cell">学習日</div>
-      </div>
-      <div v-for="(item, index) in memos.this_page_record">
-        <div class="row">
-          <div class="cell">{{ item.subject_name }}</div>
-          <div class="cell">{{ item.category }}</div>
-          <div class="cell">{{ item.used_time }}</div>
-          <div class="cell">{{ item.study_date }}</div>
-        </div>
-      </div>
-    </div> -->
     <div v-for="(item, index) in memos.this_page_record">
       <details>
         <summary>
@@ -137,15 +48,16 @@ onMounted(async () => {
     <br>
     <hr><br>
     <div class="pagination_link_list">
-      <div class="pagination_link_element" v-if="memos.max_page_num > 1" @click="update_page(memos.result_all, 1)">≪
+      <div class="pagination_link_element" v-if="memos.max_page_num > 1" @click="update_page(memos, memos.result_all, 1)">
+        ≪
       </div>
       <div v-for="page_num in memos.max_page_num">
         <div class="pagenation_plain_element" v-if="page_num == memos.open_page_num">{{ page_num }}</div>
-        <div v-else class="pagination_link_element" @click="update_page(memos.result_all, page_num)">{{ page_num }}
+        <div v-else class="pagination_link_element" @click="update_page(memos, memos.result_all, page_num)">{{ page_num }}
         </div>
       </div>
       <div class="pagination_link_element" v-if="memos.max_page_num > 1"
-        @click="update_page(memos.result_all, memos.max_page_num)">≫</div>
+        @click="update_page(memos, memos.result_all, memos.max_page_num)">≫</div>
     </div>
   </div>
 </template>
--- web/src/src/constants.ts
+++ web/src/src/constants.ts
@@ -4,3 +4,6 @@ export const URL = {
     // 学習メモ登録API
     MEMO_REGISTER: "https://ubj37r7okf64u5rszmgni6f3ke0mspzc.lambda-url.us-east-1.on.aws/",
 }
+
+// 1ページあたりの最大表示件数
+export const PAGE_LIMIT = 10
