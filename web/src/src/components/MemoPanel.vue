9eb2f9e179c5b15f6512a30fdc2a15dcd4397a7e
dy <ellendis1124@gmail.com>
2024-02-15 23:59:04 +0900

    メモ参照画面に編集ボタンと削除ボタンを追加

--- web/src/src/components/MemoPanel.vue
+++ web/src/src/components/MemoPanel.vue
@@ -13,7 +13,22 @@ const props = defineProps(['item'])
             <li><strong><span class="column_name">学習日</span></strong> {{ item.study_date }}</li>
             <li><strong><span class="column_name">学習時間</span></strong> {{ item.used_time }}</li>
             <li><strong><span class="column_name">備考</span></strong>{{ item.remarks }}</li>
+            <li>
+                <div :class="$style.icons_line">
+                    <img :class="$style.icon" src="@/assets/icon_garbagecan.svg" height="20">
+                    <img :class="$style.icon" src="@/assets/icon_edit.svg" height="20">
+                </div>
+            </li>
         </ul>
 
     </details>
 </template>
+<style module>
+.icons_line {
+    text-align: right;
+}
+
+.icon {
+    margin-left: 10px;
+}
+</style>
