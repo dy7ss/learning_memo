746fbf546f12ecbdcd6e047e33d69a7bfb5a48a7
dy <ellendis1124@gmail.com>
2024-02-22 00:42:33 +0900

    メモ削除APIの払い出し

--- web/src/src/constants.ts
+++ web/src/src/constants.ts
@@ -4,7 +4,7 @@ export const URL = {
     // 学習メモ登録API
     MEMO_REGISTER: "https://ubj37r7okf64u5rszmgni6f3ke0mspzc.lambda-url.us-east-1.on.aws/",
     // 学習メモ削除API
-    MEMO_DELETE: "https://xxxx.lambda-url.us-east-1.on.aws/",
+    MEMO_DELETE: "https://drx4u7ukt73vurzyokdjoapscu0wugjh.lambda-url.us-east-1.on.aws/",
 }
 
 // 1ページあたりの最大表示件数
